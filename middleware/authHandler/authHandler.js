bdd5d8bbfc54e19da2ddf1861ec63dc77ca9c1c8
eunxoo <dmstn7989@naver.com>
2024-03-18 08:46:12 +0900

    feat: 토큰 바디로 넘기기

--- middleware/authHandler/authHandler.js
+++ middleware/authHandler/authHandler.js
@@ -1,7 +1,14 @@
 const { parseAuthToken } = require("../../util/jwt/jwtUtil");
 
 const authHandler = (req, res, next) => {
-  const user = parseAuthToken(req.cookies.authToken);
+  let token = req.cookies.authToken;
+  let headerToken = req.headers.authorization;
+
+  // 토큰이 없고 헤더가 있는 경우
+  if (!token && headerToken) {
+    token = headerToken.split(" ")[1];
+  }
+  const user = parseAuthToken(token);
   if (user) {
     req.user = user;
     return next();
