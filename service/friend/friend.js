e290b90619b8b16f7d2410e0aa0d70c6d3dd136f
eunxoo <dmstn7989@naver.com>
2024-03-29 13:55:53 +0900

    feat: 친구 요청 거절, 친구 대기, 친구 수, 친구 검색

--- service/friend/friend.js
+++ service/friend/friend.js
@@ -72,19 +72,89 @@ const acceptFriend = async (userId, friendId) => {
   }
 };
 
+// 친구 요청 거절
+const rejectFriendRequest = async (userId, friendId) => {
+  try {
+    const user = await User.findById(userId);
+    const friendToReject = user.friend.find(
+      (friend) => friend.user.toString() === friendId
+    );
+
+    if (friendToReject && friendToReject.state === "pending") {
+      // 요청된 친구 요청을 삭제
+      await User.findByIdAndUpdate(
+        userId,
+        { $pull: { friend: { user: friendId } } },
+        { new: true }
+      );
+
+      await User.findByIdAndUpdate(
+        friendId,
+        { $pull: { friend: { user: userId } } },
+        { new: true }
+      );
+    }
+  } catch (err) {
+    throw new ApplicationError(400, "친구 요청을 거절할 수 없습니다.");
+  }
+};
+
 // 친구 목록
 const getFriendList = async (userId) => {
   try {
-    const user = await User.findOne({ _id: userId, "friend.state": "friend" })
-      .populate({
-        path: "friend.user",
-        select: "nickname",
-      })
-      .exec();
+    const user = await User.findById(userId);
+    const friendList = user.friend.filter(
+      (friend) => friend.state === "friend"
+    );
 
-    const friendList = user.friend.map((friend) => friend.user);
+    const friendIds = friendList.map((friend) => friend.user);
+    const friendsWithNicknames = await User.find({ _id: { $in: friendIds } })
+      .select("nickname") // 닉네임 필드만 선택
+      .lean(); // 결과를 plain JavaScript 객체로 변환
 
-    return friendList;
+    return friendsWithNicknames;
+  } catch (err) {
+    throw new ApplicationError(400, "친구 목록을 가져올 수 없습니다.");
+  }
+};
+
+// 친구 대기 목록
+const getPendingFriendList = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const friendList = user.friend.filter(
+      (friend) => friend.state === "pending"
+    );
+
+    const friendIds = friendList.map((friend) => friend.user);
+    const friendsWithNicknames = await User.find({ _id: { $in: friendIds } })
+      .select("nickname") // 닉네임 필드만 선택
+      .lean(); // 결과를 plain JavaScript 객체로 변환
+
+    return friendsWithNicknames;
+  } catch (err) {
+    throw new ApplicationError(400, "친구 목록을 가져올 수 없습니다.");
+  }
+};
+
+// 친구 상태 목록
+const getFriendStateList = async (userId, friendId) => {
+  try {
+    const user = await User.findById(userId);
+
+    const friend = user.friend.find(
+      (friend) => friend.user.toString() === friendId
+    );
+    if (!friend) {
+      return null; // friendId와 일치하는 친구가 없을 경우 null 반환
+    }
+
+    const friendUser = await User.findById(friend.user);
+    return {
+      friendId: friend.user,
+      nickname: friendUser.nickname,
+      state: friend.state,
+    };
   } catch (err) {
     throw new ApplicationError(400, "친구 목록을 가져올 수 없습니다.");
   }
@@ -110,10 +180,31 @@ const removeFriend = async (userId, friendId) => {
   }
 };
 
+async function getFriendCount(userId) {
+  try {
+    const user = await User.findById(userId);
+
+    if (!user) {
+      throw new Error("사용자를 찾을 수 없습니다.");
+    }
+
+    const friendCount = user.friend.filter(
+      (friend) => friend.state === "friend"
+    ).length;
+    return friendCount;
+  } catch (error) {
+    throw error;
+  }
+}
+
 module.exports = {
   requestFriend,
   acceptFriend,
   getFriendList,
   removeFriend,
   removeFriendRequest,
+  getFriendCount,
+  getPendingFriendList,
+  rejectFriendRequest,
+  getFriendStateList,
 };
