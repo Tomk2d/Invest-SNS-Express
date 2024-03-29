b409332debc0fccd9247988463b19b62ef8e1a95
eunxoo <dmstn7989@naver.com>
2024-03-29 13:54:37 +0900

    feat: 댓글, 피드 api
    피드 가져오기, 게시글 수, 글 삭제, 댓글 수

--- service/comment/comment.js
+++ service/comment/comment.js
@@ -1,16 +1,29 @@
 const formatDate = require("../../util/dateFormat/dateFormat.js");
 const ApplicationError = require("../../util/error/applicationError");
 const Comment = require("../../model/Comment");
+const User = require("../../model/User.js");
 
 const addComment = async (feedId, content, userId) => {
   try {
+    // 사용자 정보 가져오기
+    const user = await User.findById(userId);
+
+    // 댓글 생성
     const newComment = await Comment.create({
       user: userId,
       feed: feedId,
       content: content,
     });
 
-    return newComment;
+    return {
+      ...newComment._doc,
+      createdAt: formatDate(newComment.createdAt), // createdAt 포맷팅
+      user: {
+        // 사용자의 닉네임 정보 추가
+        _id: user._id,
+        nickname: user.nickname,
+      },
+    };
   } catch (err) {
     throw new ApplicationError(400, "댓글을 작성할 수 없습니다.");
   }
