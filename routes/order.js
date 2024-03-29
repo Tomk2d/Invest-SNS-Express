4011267a94a7f3567470ecbd19e80fa3f2293ebb
chaeheonjeong <cogjs3210@naver.com>
2024-03-29 13:35:32 +0900

    feat: Add holding

--- routes/order.js
+++ routes/order.js
@@ -1,19 +1,19 @@
 var express = require("express");
 var router = express.Router();
-const Order = require("../model/Order.js");
+const Order = require("../model/Holding.js");
 const authHandler = require("../middleware/authHandler/authHandler.js");
 
-router.get('/', authHandler, async(req, res, next)=>{
-    try{
-        const userId = req.user.id;
-        const myStock = await Order.findOne({user:userId});
-        const response = MyOrder(myStock);
-        res.json(response);
-    }catch(err){
-        console.error(err);
-        return next(err);
-    }
-})
+router.get("/", authHandler, async (req, res, next) => {
+  try {
+    const userId = req.user.id;
+    const myStock = await Order.findOne({ user: userId });
+    const response = MyOrder(myStock);
+    res.json(response);
+  } catch (err) {
+    console.error(err);
+    return next(err);
+  }
+});
 
 router.post("/", authHandler, async (req, res, next) => {
   try {
@@ -23,24 +23,26 @@ router.post("/", authHandler, async (req, res, next) => {
     // 기존 Order 찾기
     let order = await Order.findOne({ user: userId });
 
-        if (order) {
-            // 기존에 거래한적 있는 유저이면, stocks 배열에 주식 정보 추가
-            order.stocks.push({ ownedShare, price, quantity, Date });
-            await order.save();
-            res.status(200).json({ message: "주식 정보가 추가되었습니다.", order });
-        } else {
-            // 기존에 거래한적 없는 유저이면, 새로운 Order 생성
-            const newOrder = new Order({
-                user: userId,
-                stocks: [{ ownedShare, price, quantity, Date }]
-            });
-            await newOrder.save();
-            res.status(201).json({ message: "새로운 주문이 생성되었습니다.", newOrder });
-        }
-    } catch (err) {
-        console.error(err);
-        return next(err);
+    if (order) {
+      // 기존에 거래한적 있는 유저이면, stocks 배열에 주식 정보 추가
+      order.stocks.push({ ownedShare, price, quantity, Date });
+      await order.save();
+      res.status(200).json({ message: "주식 정보가 추가되었습니다.", order });
+    } else {
+      // 기존에 거래한적 없는 유저이면, 새로운 Order 생성
+      const newOrder = new Order({
+        user: userId,
+        stocks: [{ ownedShare, price, quantity, Date }],
+      });
+      await newOrder.save();
+      res
+        .status(201)
+        .json({ message: "새로운 주문이 생성되었습니다.", newOrder });
     }
+  } catch (err) {
+    console.error(err);
+    return next(err);
+  }
 });
 
 router.post("/stock", authHandler, async (req, res, next) => {
@@ -70,3 +72,5 @@ router.get("/myHistory/:code", authHandler, async (req, res, next) => {
     return next(err);
   }
 });
+
+module.exports = router;
--- service/order/processOrder.js
+++ service/order/processOrder.js
@@ -1,89 +1,81 @@
 const { promisify } = require("util");
-const Order = require("../../model/Order");
+const Holding = require("../../model/Holding");
 const UnfilledOrder = require("../../model/UnfilledOrder");
 
 async function processOrder(data) {
-  try {
-    // data : {code: "", sellPrice: [], buyPrice: []}
-    const unfilledOrders = await UnfilledOrder.find({ ownedShare: data.code });
-
-    for (const order of unfilledOrders) {
-      const { buyOrSell, price } = order;
-
-      if (buyOrSell === "buy") {
-        if (data.sellPrice[0] <= price) {
-          const buyPrice =
-            data.sellPrice[0] === price ? price : data.sellPrice[0];
-          const existingOrder = await Order.findOne({ user: order.user });
-
-          if (existingOrder) {
-            existingOrder.stocks.push({
-              ownedShare: data.code,
-              price: buyPrice,
-              quantity: order.quantity,
-              buyOrSell: buyOrSell,
-              time: order.time,
-            });
-            await existingOrder.save();
-          } else {
-            const newOrder = new Order({
-              user: order.user,
-              stocks: [
-                {
-                  ownedShare: data.code,
-                  price: buyPrice,
-                  quantity: order.quantity,
-                  buyOrSell: buyOrSell,
-                  time: order.time,
-                },
-              ],
-            });
-            await newOrder.save();
-          }
-
-          console.log(order._id);
-
-          await UnfilledOrder.findByIdAndDelete(order._id);
-        }
-      } else if (buyOrSell === "sell") {
-        if (data.buyPrice[0] >= price) {
-          const sellPrice =
-            data.buyPrice[0] === price ? price : data.buyPrice[0];
-          //매도 가격
-
-          const existingOrder = await Order.findOne({
-            user: order.user,
-            "stocks.ownedShare": data.code,
-          });
-
-          if (existingOrder) {
-            let remainQuantity = order.quantity;
-            for (let i = 0; i < existingOrder.stocks.length; i++) {
-              if (remainQuantity > 0) {
-                if (existingOrder.stocks[i].quantity >= remainQuantity) {
-                  existingOrder.stocks[i].quantity -= remainQuantity;
-                  remainQuantity = 0;
-
-                  if (existingOrder.stocks[i].quantity === 0) {
-                    existingOrder.stocks.splice(i, 1);
-                  }
-                } else {
-                  remainQuantity -= existingOrder.stocks[i].quantity;
-                  existingOrder.stocks.splice(i, 1);
-                }
-              } else {
-                break;
-              }
-            }
-            await existingOrder.save();
-          }
-          await UnfilledOrder.findByIdAndDelete(order._id);
-        }
-      }
-    }
-  } catch (error) {
-    console.error("Error while processing stock codes:", error);
-  }
+  // try {
+  //   // data : {code: "", sellPrice: [], buyPrice: []}
+  //   const unfilledOrders = await UnfilledOrder.find({ ownedShare: data.code });
+  //   for (const order of unfilledOrders) {
+  //     const { buyOrSell, price } = order;
+  //     if (buyOrSell === "buy") {
+  //       if (data.sellPrice[0] <= price) {
+  //         const buyPrice =
+  //           data.sellPrice[0] === price ? price : data.sellPrice[0];
+  //         const existingOrder = await Order.findOne({ user: order.user });
+  //         if (existingOrder) {
+  //           existingOrder.stocks.push({
+  //             ownedShare: data.code,
+  //             price: buyPrice,
+  //             quantity: order.quantity,
+  //             buyOrSell: buyOrSell,
+  //             time: order.time,
+  //           });
+  //           await existingOrder.save();
+  //         } else {
+  //           const newOrder = new Order({
+  //             user: order.user,
+  //             stocks: [
+  //               {
+  //                 ownedShare: data.code,
+  //                 price: buyPrice,
+  //                 quantity: order.quantity,
+  //                 buyOrSell: buyOrSell,
+  //                 time: order.time,
+  //               },
+  //             ],
+  //           });
+  //           await newOrder.save();
+  //         }
+  //         console.log(order._id);
+  //         await UnfilledOrder.findByIdAndDelete(order._id);
+  //       }
+  //     } else if (buyOrSell === "sell") {
+  //       if (data.buyPrice[0] >= price) {
+  //         const sellPrice =
+  //           data.buyPrice[0] === price ? price : data.buyPrice[0];
+  //         //매도 가격
+  //         const existingOrder = await Order.findOne({
+  //           user: order.user,
+  //           "stocks.ownedShare": data.code,
+  //         });
+  //         if (existingOrder) {
+  //           let remainQuantity = order.quantity;
+  //           for (let i = 0; i < existingOrder.stocks.length; i++) {
+  //             if (remainQuantity > 0) {
+  //               if (existingOrder.stocks[i].quantity >= remainQuantity) {
+  //                 existingOrder.stocks[i].quantity -= remainQuantity;
+  //                 remainQuantity = 0;
+  //                 if (existingOrder.stocks[i].quantity === 0) {
+  //                   existingOrder.stocks.splice(i, 1);
+  //                 }
+  //               } else {
+  //                 remainQuantity -= existingOrder.stocks[i].quantity;
+  //                 existingOrder.stocks.splice(i, 1);
+  //               }
+  //             } else {
+  //               break;
+  //             }
+  //           }
+  //           await existingOrder.save();
+  //         }
+  //         await UnfilledOrder.findByIdAndDelete(order._id);
+  //       }
+  //     }
+  //   }
+  // } catch (error) {
+  //   console.error("Error while processing stock codes:", error);
+  // }
 }
 
 module.exports = { processOrder };
